4bac76fc95107974df37e1d5cfa5fd5dcb311e1c
Kabiswa Davis <kabiswad@gmail.com>
2026-01-09 21:34:32 +0200

    add the scanning

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::env;
-use std::net::IpAddr;
+use std::io::{self, Write};
+use std::net::{IpAddr, TcpStream};
+use std::process;
 use std::str::FromStr;
+use std::sync::mpsc::{Sender, channel};
+use std::thread;
+
+const MAX: u16 = 65535;
 
 #[derive(Debug)]
 struct Arguments {
-    flag: String,
     ipaddr: IpAddr,
     threads: u16,
 }
@@ -24,11 +29,7 @@ impl Arguments {
             _ => {
                 let ipaddr = IpAddr::from_str(arg).map_err(|_| "not a valid IP address")?;
 
-                Ok(Self {
-                    flag: String::from(""),
-                    ipaddr,
-                    threads: 4,
-                })
+                Ok(Self { ipaddr, threads: 4 })
             }
         }
     }
@@ -44,28 +45,67 @@ impl Arguments {
 
         let ipaddr = IpAddr::from_str(&args[2]).map_err(|_| "not a valid IP address")?;
 
-        Ok(Self {
-            flag: args[0].clone(),
-            ipaddr,
-            threads,
-        })
+        Ok(Self { ipaddr, threads })
+    }
+}
+
+fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
+    let mut port: u16 = start_port + 1;
+    loop {
+        match TcpStream::connect((addr, port)) {
+            Ok(_) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
+            Err(_) => {}
+        }
+
+        if (MAX - port) <= num_threads {
+            break;
+        }
+        port += num_threads;
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    match Arguments::new(&args) {
-        Ok(config) => {
-            println!("{:?}", config);
-        }
+    let program = args[0].clone();
+    let arguments = match Arguments::new(&args) {
+        Ok(config) => config,
         Err("help") => {
             println!("Usage:");
             println!("  scanner <IP>");
             println!("  scanner -j <THREADS> <IP>");
+            process::exit(0);
         }
-        Err(e) => {
-            eprintln!("Error: {}", e);
+        Err(err) => {
+            eprintln!("{} problem parsing arguments: {}", program, err);
+            process::exit(1);
         }
+    };
+
+    let num_threads = arguments.threads;
+    let addr = arguments.ipaddr;
+    let (tx, rx) = channel();
+    for i in 0..num_threads {
+        let tx = tx.clone();
+
+        thread::spawn(move || {
+            scan(tx, i, addr, num_threads);
+        });
+    }
+
+    let mut out = vec![];
+    drop(tx);
+    for p in rx {
+        out.push(p);
+    }
+
+    println!("");
+    out.sort();
+    for v in out {
+        println!("{} is open", v);
     }
 }
